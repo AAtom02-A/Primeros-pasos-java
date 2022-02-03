0aef88e22f48eb2b6d1ad05595a34820ec21e558
Angel Alvarez <tonatiuh1099@gmail.com>
2022-02-03 12:53:08 -0600

    Se agrega el proyecto NumeroParImpar

---
+++ NumeroParImpar/src/NumeroParImpar.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class NumeroParImpar {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+/*Crear un nuevo proyecto llamado NumeroParImpar
+crear el método main
+crear una función que evalúe si un número es par o impar (se puede usar el operador ternario)
+Imprimir en terminal el resultado
+
+*extra, investigar cómo usar la clase Scanner en java.
+
+		
+		
+
+		
+		    
+		        int numero;
+		       Scanner teclado = new Scanner( System.in );
+
+		        System.out.printf( "Introduzca un número entero: " );
+		        numero = teclado.nextInt();
+
+		       if ( numero % 2 == 0 )
+		        {
+		            System.out.println( "ES PAR" );
+		        }
+		        else
+		        {
+		            System.out.println( "ES IMPAR" );
+		        }
+		        
+		      //  String resultado = ((numero % 2 ==0 ) ? "par" : "impar");
+		        //System.out.println(resultado);	
+		       
+		       //Scanner se utiliza para recolectar datos por teclado del usuario.
+		       
+		       Scanner sc = new Scanner (System.in);
+		       System.out.println("Escribe un numero: cx");
+		       int num = sc.nextInt();
+		       //String nu = sc.nextLine(); nextLine es para string
+		       System.out.print(num);
+		
+
+}*/
+}
